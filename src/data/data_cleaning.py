69d9106c7e8f28203d0d9d8bf751110d57648087
Romuald86github <romualdtcheutchoua@gmail.com>
2024-07-12 23:03:58 +0100

    update

--- src/data/data_cleaning.py
+++ src/data/data_cleaning.py
@@ -2,16 +2,14 @@ import os
 import pandas as pd
 import numpy as np
 import requests
-from io import StringIO
-from mage_ai.data_preparation.decorators import data_loader, data_exporter
 
-@data_loader
 def load_data(url):
-    """Load the raw data from the provided URL and save it to the 'data_loaders/raw_data.csv' file."""
+    """Load the raw data from the provided URL and save it to the 'data/raw/raw_data.csv' file."""
     response = requests.get(url)
 
-    # Save the raw data to 'data_loaders/raw_data.csv'
-    file_path = os.path.join('my-attrition-prediction-project', 'data_loaders', 'raw_data.csv')
+    # Save the raw data to 'data/raw/raw_data.csv'
+    file_path = os.path.join('data', 'raw', 'raw_data.csv')
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, 'wb') as f:
         f.write(response.content)
 
@@ -20,16 +18,15 @@ def load_data(url):
 
     return data
 
-@data_exporter
 def clean_data(data):
-    """Clean the raw data."""
+    """Clean the raw data and save it to 'data/processed/cleaned_data.csv'."""
     # (1) Convert 'MonthlyIncome' and 'MonthlyRate' columns to float
     data[['MonthlyIncome', 'MonthlyRate']] = data[['MonthlyIncome', 'MonthlyRate']].astype(float)
 
     # (2) Convert specified columns to object data type
     columns_to_convert = ['Education', 'EnvironmentSatisfaction', 'JobInvolvement', 'JobLevel', 'JobSatisfaction',
-                         'NumCompaniesWorked', 'PerformanceRating', 'RelationshipSatisfaction', 'StockOptionLevel',
-                         'TrainingTimesLastYear', 'WorkLifeBalance']
+                           'NumCompaniesWorked', 'PerformanceRating', 'RelationshipSatisfaction', 'StockOptionLevel',
+                           'TrainingTimesLastYear', 'WorkLifeBalance']
     data[columns_to_convert] = data[columns_to_convert].astype(object)
 
     # (3) Set 'EmployeeNumber' as the index
@@ -41,7 +38,8 @@ def clean_data(data):
     data = data[(z_scores <= 3).all(axis=1)]
 
     # (5) Save the cleaned data
-    file_path = os.path.join('my-attrition-prediction-project', 'transformers', 'clean_data.csv')
+    file_path = os.path.join('data', 'processed', 'cleaned_data.csv')
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
     data.to_csv(file_path)
 
     return data
@@ -50,4 +48,3 @@ if __name__ == "__main__":
     url = "https://raw.githubusercontent.com/Romuald86github/Internship/main/employee_attrition.csv"
     raw_data = load_data(url)
     cleaned_data = clean_data(raw_data)
-
