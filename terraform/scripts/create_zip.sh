56916c74af8c39492fe1ddaaa425bec3bf679411
Romuald86github <romualdtcheutchoua@gmail.com>
2024-07-25 17:36:56 +0100

    updates

--- terraform/scripts/create_zip.sh
+++ terraform/scripts/create_zip.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Navigate to the app directory
-cd ../app
+# Navigate to the project root directory
+cd ../
 
 # Zip the application files
-zip -r ../terraform/app.zip .
+zip -r terraform/app.zip app/
 
-# Navigate back to the terraform directory
-cd ../terraform
+# Navigate back to the terraform/scripts directory
+cd terraform/scripts
